7c2e2540ae4fb23d651a5d41e8fda9f09decb42c
aboghosiansite <78941671+aboghosiansite@users.noreply.github.com>
2021-02-11 20:27:16 -0500

    initial push

---
+++ aboghosiansite.github.io/script.js
@@ -0,0 +1,23 @@
+function autorun() {
+
+}
+if (document.addEventListener) document.addEventListener("DOMContentLoaded", autorun, false);
+else if (document.attachEvent) document.attachEvent("onreadystatechange", autorun);
+else window.onload = autorun;
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    console.log('hi')
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
